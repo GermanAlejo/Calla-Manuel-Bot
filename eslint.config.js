46a36d10c5c5d2948dbef9f2892fcf1ef60cc649
Germán Alejo <geralejo95@gmail.com>
2024-12-30 21:44:54 +0100

    EsLint reconfigured
    EsLint was reconfigured so less rules would conflict with the grammY code

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,8 @@
 import tseslint from 'typescript-eslint';
 import typescriptPlugin from '@typescript-eslint/eslint-plugin';
 import typescriptParser from '@typescript-eslint/parser';
+import prettierPlugin from "eslint-plugin-prettier";
+import importPlugin from "eslint-plugin-import";
 
 /* eslint-env node */
 export default [
@@ -19,7 +21,7 @@ export default [
         project: true,
         tsconfigRootDir: import.meta.dirname,
       },
-      globals: { 
+      globals: {
         __dirname: "readonly",
         process: "readonly",
         __filename: "readonly"
@@ -28,49 +30,55 @@ export default [
     // This is needed in order to specify the desired behavior for its rules
     plugins: {
       '@typescript-eslint': typescriptPlugin,
+      'prettier': prettierPlugin,
+      'import': importPlugin
     },
-
     // After defining the plugin, you can use the rules like this
     rules: {
-      'no-unused-vars': 'error',
-      'no-console': 'warn',
       'no-undef': 'warn',
-      'quotes': 'off',
       'import/no-unresolved': 'off',
-      'indent': 'off',
-      'no-array-constructor': 'off',
       'complexity': ['error', { 'max': 10 }],
       //Typescript rules
-      '@typescript-eslint/no-unnecessary-condition': 'error',
-      '@typescript-eslint/naming-convention': 'warn',
-      '@typescript-eslint/no-floating-promises': 'error',
-      '@typescript-eslint/no-shadow': 'error',
-      '@typescript-eslint/ban-ts-comment': 'error',
-      '@typescript-eslint/no-array-constructor': 'error',
-      '@typescript-eslint/no-duplicate-enum-values': 'error',
-      '@typescript-eslint/no-empty-object-type': 'error',
-      '@typescript-eslint/no-explicit-any': 'error',
-      '@typescript-eslint/no-extra-non-null-assertion': 'error',
-      '@typescript-eslint/no-misused-new': 'error',
-      '@typescript-eslint/no-namespace': 'error',
-      '@typescript-eslint/no-non-null-asserted-optional-chain': 'error',
       '@typescript-eslint/no-require-imports': 'error',
-      '@typescript-eslint/no-this-alias': 'error',
-      '@typescript-eslint/no-unnecessary-type-constraint': 'error',
-      '@typescript-eslint/no-unsafe-declaration-merging': 'error',
       '@typescript-eslint/no-unsafe-function-type': 'error',
       '@typescript-eslint/no-unused-expressions': 'error',
-      '@typescript-eslint/no-unused-vars': 'error',
-      '@typescript-eslint/no-wrapper-object-types': 'error',
-      '@typescript-eslint/prefer-as-const': 'error',
-      '@typescript-eslint/prefer-namespace-keyword': 'error',
-      '@typescript-eslint/triple-slash-reference': 'error',
+      '@typescript-eslint/no-array-constructor': 'error',
       '@typescript-eslint/no-base-to-string': 'warn',
       '@typescript-eslint/no-array-delete': 'error',
       '@typescript-eslint/await-thenable': 'error',
-      '@typescript-eslint/no-unsafe-call': 'off',
+      '@typescript-eslint/no-unsafe-call': 'error',
       '@typescript-eslint/no-unsafe-member-access': 'off',
-      '@typescript-eslint/no-unsafe-enum-comparison': 'warn'
+      'prettier/prettier': [
+        'off',//THIS COUSES SEVERAL ERRORS WHICH ARE NOT FIXABLE
+        {
+          endOfLine: 'auto', // Align with Prettier configuration
+        },
+      ],
+      '@typescript-eslint/no-floating-promises': 'error',
+      '@typescript-eslint/no-misused-promises': [
+        'error',
+        {
+          checksVoidReturn: false,
+        },
+      ],
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/consistent-type-imports': 'error',
+      'no-console': [
+        'warn',
+        { allow: ['warn', 'error'] }
+      ],
+      'import/no-unused-modules': ["warn"],
+      'import/order': [
+        "warn",
+        {
+          'groups': [
+            ['builtin', 'external'],
+            'internal',
+            ['parent', 'sibling', 'index']
+          ],
+          'newlines-between': 'always',
+        },
+      ],
     }
   }
 ];
